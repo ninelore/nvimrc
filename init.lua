098c9da833fb7bebd86044b4ee933618815fc455
Ingo Reitz <9l@9lo.re>
2025-01-14 20:15:31 +0100

    new theme: monokai-nightasty

--- init.lua
+++ init.lua
@@ -446,12 +446,28 @@ require("lazy").setup({
 		end,
 	},
 
-	{
+	-- THEME
+	--[[{
 		"loctvl842/monokai-pro.nvim",
 		priority = 1000,
 		init = function()
 			vim.cmd.colorscheme("monokai-pro")
 			vim.cmd.hi("Comment gui=none")
+		end,
+	}, -- ]]
+	{
+		"polirritmico/monokai-nightasty.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			dark_style_background = "dark", -- default | dark | transparent | #RRGGBB
+			markdown_header_marks = true,
+			terminal_colors = function(colors)
+				return { fg = colors.fg_dark }
+			end,
+		},
+		config = function(_, opts)
+			require("monokai-nightasty").load(opts)
 		end,
 	},
 
