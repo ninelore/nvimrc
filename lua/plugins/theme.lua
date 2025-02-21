d3e13521d9a180b2be5c681d40328e7fd11fac9d
Ingo Reitz <9l@9lo.re>
2025-02-21 13:29:04 +0100

    update, new theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,17 +1,30 @@
 return {
+	-- {
+	-- 	"polirritmico/monokai-nightasty.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {
+	-- 		dark_style_background = "dark", -- default | dark | transparent | #RRGGBB
+	-- 		markdown_header_marks = true,
+	-- 		terminal_colors = function(colors)
+	-- 			return { fg = colors.fg_dark }
+	-- 		end,
+	-- 		on_colors = function(colors) end,
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		require("monokai-nightasty").load(opts)
+	-- 	end,
+	-- },
 	{
-		"polirritmico/monokai-nightasty.nvim",
+		"sainnhe/sonokai",
 		lazy = false,
 		priority = 1000,
-		opts = {
-			dark_style_background = "dark", -- default | dark | transparent | #RRGGBB
-			markdown_header_marks = true,
-			terminal_colors = function(colors)
-				return { fg = colors.fg_dark }
-			end,
-		},
-		config = function(_, opts)
-			require("monokai-nightasty").load(opts)
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.sonokai_enable_italic = true
+			vim.g.sonokai_style = "shusia"
+			vim.cmd.colorscheme("sonokai")
 		end,
 	},
 }
