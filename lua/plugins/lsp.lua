910f5dd28b299480d92a284ab912881dcf0acf1e
Ingo Reitz <9l@9lo.re>
2025-05-12 07:03:31 +0200

    conform: add disable command

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -211,6 +211,9 @@ return {
 			require("conform").setup({
 				notify_on_error = false,
 				format_on_save = function(bufnr)
+					if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+						return
+					end
 					local disable_filetypes = { c = true, cpp = true }
 					local lsp_format_opt
 					if disable_filetypes[vim.bo[bufnr].filetype] then
@@ -227,6 +230,24 @@ return {
 					lua = { "stylua" },
 					nix = { "nixfmt" },
 				},
+			})
+
+			vim.api.nvim_create_user_command("FormatDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+				bang = true,
+			})
+			vim.api.nvim_create_user_command("FormatEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
 			})
 
 			require("mason-conform").setup()
