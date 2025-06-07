46bb36f5503e5208c04b4886bddb22f5543dc358
Ingo Reitz <9l@9lo.re>
2025-06-07 20:30:55 +0200

    nvim-treesitter: fixes after breaking changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,37 @@
 return {
-	{ -- Highlight, edit, and navigate code
+	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		branch = "main",
 		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"gitcommit",
-				"gitignore",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true,
 			highlight = {
 				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				additional_vim_regex_highlighting = { "ruby" },
 			},
-			indent = { enable = true, disable = { "ruby" } },
+		},
+		config = function()
+			vim.bo.indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+		end,
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(args)
+					local buf = args.buf
+					local ft = vim.bo[buf].filetype
+					local lang = vim.treesitter.language.get_lang(ft) or ft
+					if vim.list_contains(require("nvim-treesitter").get_installed(), lang) then
+						pcall(vim.treesitter.start, buf, lang)
+					end
+				end,
+			})
+		end,
+		dependencies = {
+			{
+				"lewis6991/ts-install.nvim",
+				config = function()
+					require("ts-install").setup({
+						auto_install = true,
+					})
+				end,
+			},
 		},
 	},
 }
